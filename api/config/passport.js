aae2df335f07e90a58c6f367ee92f1a597f46438
psbadewal <psbadewal@icloud.com>
2015-12-13 18:30:13 +0000

    Defining local-strategy signup fields in config/passport

--- api/config/passport.js
+++ api/config/passport.js
@@ -2,6 +2,30 @@ var LocalStrategy = require("passport-local").Strategy;
 var User = require('../models/user');
 
 module.exports = function(passport) {
-  
-}
 
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: "email",
+    passportField: "password",
+    passReqToCallBack: true, 
+  }, function(req, email, password, done) {
+
+    User.findOne({ 'local.email' : email }, function(err, user) {
+      if (err) return done(err, false, { message: 'Something went wrong' });
+
+      if (user) return done(null, false, { message: 'Please choose another email' });
+
+      var newUser = new User();
+      newUser.local.email = email;
+      newUser.local.username = req.body.username;
+      newUser.local.fullname = req.body.fullname;
+      newUser.local.password = User.encrypt(password);
+      newUser.save (function(err, user) {
+        console.log(err)
+        if (err) return done(err, false, { message: "Something went wrong." });
+
+        return done(null, user);
+      }) 
+    });
+  }));
+
+}
