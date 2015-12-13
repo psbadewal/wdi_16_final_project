62aeb154bc317eaae4a48130e3f0e4cd47c23a50
psbadewal <psbadewal@icloud.com>
2015-12-13 18:49:01 +0000

    Adding login function and module.exports in authenticationsController

--- api/controllers/authenticationsController.js
+++ api/controllers/authenticationsController.js
@@ -1,8 +1,8 @@
 var passport = require('passport');
-var User = require('../models/user');
+var User     = require('../models/user');
 
-var secret = require('../config/config').secret;
-var jwt = require('jsonwebtoken');
+var secret   = require('../config/config').secret;
+var jwt      = require('jsonwebtoken');
 
 function register(req, res, next) {
   var localStrategy = passport.authenticate('local-signup', function(err, user, info) {
@@ -20,8 +20,29 @@ function register(req, res, next) {
     });
   });
 
+  return localStrategy(req, res, next);
+};
 
+function login(req, res, next) {
+  User.findOne({
+    "local.email": req.body.email
+  }, function(err, user) {
+    if (err) return res.status(500).json(err);
+    if (!user) return res.status(403).json({ message: 'No user found' });
+    if(!user.validPassword(req.body.password) return res.status(403).json({ message: 'Authentication failed.' });
 
+      var token = jwt.sign(user, secret, { expiresIn: 60*60*24 });
 
-  
-}
+      return res.status(200).json({
+        success: true,
+        message: "Welcome!",
+        token: token,
+        user: user
+      });
+    });
+  };
+
+  module.exports = {
+    login: login,
+    register: register
+  }
