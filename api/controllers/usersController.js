06baf991ac7d879f92f9e98e73e33ee905dd27da
psbadewal <psbadewal@icloud.com>
2015-12-13 17:59:05 +0000

    Inserting module.exports in controllers/usersController

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -2,7 +2,7 @@ var User = require('../models/user');
 
 function usersIndex(req, res) {
   User.find(function(err, users){
-    if(err) return res.status(404).json({
+    if (err) return res.status(404).json({
       message: 'Something went wrong' });
       res.status(200).json({ users: users });
   });
@@ -18,15 +18,14 @@ function usersShow(req, res) {
 
 function usersUpdate(req, res){
   User.findById(req.params.id, function(err, user) {
-    if (err) return res.status(500).json({ message: 'Something went wrong!'});
+    if (err) return res.status(500).json({ message: 'Something went wrong!' });
     if (!user) return res.status(404).json({ message: 'No user found' });
 
     if (req.body.email) user.local.email = req.body.name;
     if (req.body.password) user.local.password = req.body.password;
 
     user.save(function(err) {
-      if (err) return res.status(500).json({
-        message: 'Something went wrong!' });
+      if (err) return res.status(500).json({ message: 'Something went wrong!' });
 
         res.status(201).json({ message: 'User successfully updated.', user: user });
     });
@@ -35,7 +34,14 @@ function usersUpdate(req, res){
 
 function usersDelete(req, res) {
   User.findByIdAndRemove({_id: req.params.id}, function(err){
-    if(err) return res.status(404).json({ message: 'Something went wrong.'});
-    res.status(200).json({ message: 'User has been successfully deleted'});
+    if (err) return res.status(404).json({ message: 'Something went wrong.'});
+    res.status(200).json({ message: 'User has been successfully deleted' });
   });
+}
+
+module.exports = {
+  usersIndex: usersIndex,
+  usersShow: usersShow,
+  usersUpdate: usersUpdate,
+  usersDelete: usersDelete;
 }
