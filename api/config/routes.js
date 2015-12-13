12780fbef94b1c869453914649581699832f295b
psbadewal <psbadewal@icloud.com>
2015-12-13 18:07:07 +0000

    Inserting routes for users in config/routes

--- api/config/routes.js
+++ api/config/routes.js
@@ -1,2 +1,18 @@
 var express = require('express'),
     router = express.Router();
+
+    var usersController = require('../controllers/usersController');
+
+
+    router.route('/')
+      .get(usersController.usersIndex)
+
+      router.route('/users')
+      .get(usersController.usersIndex)
+
+      router.route('/users/:id')
+      .get(usersController.usersShow)
+      .patch(usersController.usersUpdate)
+      .delete(usersController.usersDelete)
+
+module.exports = router;
