289f2e1192667559396b18c895d0b488fa46c181
psbadewal <psbadewal@icloud.com>
2015-12-13 18:40:06 +0000

    Adding register function in controllers/authenticationsController

--- api/controllers/authenticationsController.js
+++ api/controllers/authenticationsController.js
@@ -4,4 +4,24 @@ var User = require('../models/user');
 var secret = require('../config/config').secret;
 var jwt = require('jsonwebtoken');
 
+function register(req, res, next) {
+  var localStrategy = passport.authenticate('local-signup', function(err, user, info) {
+    if(err) return res.status(500).json({ message: "Something went wrong!" });
+    if (info) return res.status(401).json({ message: info.message });
+    if(!user) return res.status(401).json({ message: 'User already exists!' });
 
+    var token = jwt.sign(user, secret, { expiresIn: 60*60*24 });
+
+    return res.status(200).json({
+      success: true,
+      message: "Thank you for authenticating", 
+      token: token,
+      user: user
+    });
+  });
+
+
+
+
+  
+}
