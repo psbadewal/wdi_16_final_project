320ea01064ca45171c1b5195046ffe376c95234b
psbadewal <psbadewal@icloud.com>
2015-12-13 17:27:11 +0000

    Requiring other files from api in app.js

--- api/app.js
+++ api/app.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var cors    = require('cors');
-var path    = require('path');
-var morgan  = require('morgan');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var cookieParser = require('cookie-parser');
+//Requiring node modules
+var express        = require('express');
+var cors           = require('cors');
+var path           = require('path');
+var morgan         = require('morgan');
+var body-parser    = require('body-parser');
+var mongoose       = require('mongoose');
+var passport       = require('passport');
+var cookieParser   = require('cookie-parser');
 var methodOverride = require('methodOverride');
-var jwt = require('jsonwebtoken');
-var expressJWT = require('express-jwt');
-var app = express();
+var jwt            = require('jsonwebtoken');
+var expressJWT     = require('express-jwt');
+var app            = express();
 
+//Requiring files 
+var config         = require('./config/config');
+var User           = require('./models/user');
+var secret         = require('.config/config').secret;
