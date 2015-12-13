f783a0e6a7e5eb1926d2d4f111127659193c3df5
psbadewal <psbadewal@icloud.com>
2015-12-13 17:48:15 +0000

    Inserting function usersShow in controllers/usersController

---
+++ api/controllers/usersController.js
@@ -0,0 +1,17 @@
+var User = require('../models/user');
+
+function usersIndex(req, res) {
+  User.find(function(err, users){
+    if(err) return res.status(404).json({
+      message: 'Something went wrong' });
+      res.status(200).json({ users: users });
+  });
+}
+
+function usersShow(req, res) {
+  User.findById(req.params.id, function(err, user){
+    if (err) return res.status(404).json({
+      message: 'Something went wrong.'});
+      res.status(200).json({ user: user });
+    });
+}
