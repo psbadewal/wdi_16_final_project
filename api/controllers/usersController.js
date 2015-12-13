1e06ce22422ac066a411772b43c3d31d8dca5fd6
psbadewal <psbadewal@icloud.com>
2015-12-13 17:53:33 +0000

    Inserting function userUpdate in controllers/usersController

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -15,3 +15,20 @@ function usersShow(req, res) {
       res.status(200).json({ user: user });
     });
 }
+
+function usersUpdate(req, res){
+  User.findById(req.params.id, function(err, user) {
+    if (err) return res.status(500).json({ message: 'Something went wrong!'});
+    if (!user) return res.status(404).json({ message: 'No user found' });
+
+    if (req.body.email) user.local.email = req.body.name;
+    if (req.body.password) user.local.password = req.body.password;
+
+    user.save(function(err) {
+      if (err) return res.status(500).json({
+        message: 'Something went wrong!' });
+
+        res.status(201).json({ message: 'User successfully updated.', user: user });
+    });
+  });
+}
