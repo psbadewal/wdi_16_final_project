528f7053a0a812468a838d36d94036814e3efd02
psbadewal <psbadewal@icloud.com>
2015-12-14 12:25:23 +0000

    Inserting code and injecting TokenService for currentUser.js

---
+++ front-end/js/services/currentUser.js
@@ -0,0 +1,22 @@
+angular
+.module('finalProject')
+.service('CurrentUser', CurrentUser);
+
+CurrentUser.$inject = ['TokenService']
+function CurrentUser(TokenService){
+  var self  = this;
+  self.user = {}
+
+  self.saveUser = function(user){
+    self.user   = user
+  }
+
+  self.getUser = function(){
+    return self.user;
+  }
+
+  self.clearUser = function(){
+    return self.user = {};
+  }
+
+}
