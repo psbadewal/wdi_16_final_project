c25837b215e00ade455e9e7e227a3288dba280e9
psbadewal <psbadewal@icloud.com>
2015-12-14 00:24:54 +0000

    Injected user with  and api

--- front-end/js/models/user.js
+++ front-end/js/models/user.js
@@ -1,5 +1,24 @@
 angular
-  .module('finalProject')
-  .factory('User', User)
+.module('finalProject')
+.factory('User', User)
 
-  User.$inject = ['$resource', 'API']
+User.$inject = ['$resource', 'API']
+function User($resouce, API){
+  return $resource(
+    API + '/users/:id',
+    {id: '@id'},
+    { 'get':     { method: 'GET' },
+    'save':    { method: 'POST' },
+    'query':   { method: 'GET', isArray: false },
+    'remove':  { method: 'DELETE' },
+    'delete':  { method: 'DELETE' },
+    'register':{ 
+     url: API + '/register',
+     method: 'POST' },
+     'login':  { 
+       url: API + '/login',
+       method: 'POST'
+     },
+   }
+  );
+}
