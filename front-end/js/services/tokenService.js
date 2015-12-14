bc9354221331703f97d0a865ff8c89afd7348d08
psbadewal <psbadewal@icloud.com>
2015-12-14 12:18:20 +0000

    Inserting code in tokenService.js to set, get, remove and decode token

---
+++ front-end/js/services/tokenService.js
@@ -0,0 +1,26 @@
+angular
+.module('finalProject')
+.service('TokenService', TokenService);
+
+TokenService.$inject = ['$window', 'jwtHelper'];
+function TokenService($window, jwtHelper) {
+  var self = this;
+
+  self.setToken    = setToken;
+  self.getToken    = getToken;
+  self.removeToken = removeToken;
+  self.decodeToken = decodeToken;
+
+  function setToken(token) {
+    return $window.localStorage.setItem('auth-token', token);
+  }
+
+  function removeToken(){
+    return $window.localStorage.removeItem('auth-token');
+  }
+
+  function decodeToken(){
+    var token = self.getToken();
+    return token ? jwtHelper.decodeToken(token) : {};
+  }
+}
