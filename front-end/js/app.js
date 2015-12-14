1426d2d31f1057b3e49c06d9fb724f9c6c6287d3
psbadewal <psbadewal@icloud.com>
2015-12-14 00:11:02 +0000

    Setting up Main Router with stateProvider and urlRouterProvider

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -5,3 +5,32 @@ angular
   .config(function($httpProvider){
     $httpProvider.interceptors.push('authInterceptor')
   });
+
+  MainRouter.$inject = ['
+  $stateProvider', 
+  '$urlRouterProvider'];
+
+  function MainRouter(
+    $stateProvider,
+    $urlRouterProvider) {
+    $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'home.html'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'register.html'
+    })
+    .state('profile', {
+      url: '/users',
+      templateUrl: 'users.html'
+    })
+    .state('users', {
+      url: '/users',
+      templateUrl: 'users.html'
+    })
+
+    $urlRouterProvider.
+    otherwise('/')
+  }
