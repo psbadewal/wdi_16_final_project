d9f6f77176843d314fb169812352a9934e7bf495
psbadewal <psbadewal@icloud.com>
2015-12-16 21:20:13 +0000

    Working functionality of splicing petitions array in petitions.html

--- front-end/js/controllers/petitionsController.js
+++ front-end/js/controllers/petitionsController.js
@@ -1,14 +1,45 @@
 angular
-  .module('finalproject')
-  .controller('PetitionsController', PetitionsController);
+.module('finalproject')
+.controller('PetitionsController', PetitionsController);
 
-  PetitionsController.$inject = ['Petition', '$state'];
-  function PetitionsController(Petition, $state){
+PetitionsController.$inject = ['Petition', '$state'];
+function PetitionsController(Petition, $state){
 
-    var self = this;
+  var self = this;
 
-    self.all = Petition.query();
-    self.petition = {};
+  self.all = Petition.query();
+  self.petition = {};
+
+  self.nextPetition = function(index,isSigned) {
+    // console.log(self.all[0]);
+    self.all.splice(index, 1);
+    // console.log(self.all[0]);
+    if (isSigned) {
+      //do the signing logic here
+      // 'Thanks for signing!'
+      // 'Confirmation with the Database end'
+    }
+
+  }
+  // self.currentPetition = petitions[petitionIndex];
+  // self.rejectThis = rejectThis;
+  // self.signThis   = signThis;
+
+  // this.nextPetition = function() {
+  //   petitionIndex = petitionIndex+1;
+  //   this.currentPetition = petitions[petitionIndex];
+  //   };
+
+
+
+    // function rejectThis(petitionId){
+    //   //
+    // }
+
+    // function signThis(petitionId){
+    //   petitionId =+ 1
+    // }
+
     // self.getPetitions = getPetitions;
   
   // function getPetitions() {
