b80dd8d5c8c12c32574c782d749e20e45fafdcbd
psbadewal <psbadewal@icloud.com>
2016-01-07 17:50:38 +0000

    Adding Mobile Angular UI carousel and changing user.js ft and be

--- api/models/user.js
+++ api/models/user.js
@@ -4,9 +4,16 @@ var bcrypt = require('bcrypt-nodejs');
 var userSchema = mongoose.Schema({
   local: {
     username: { type: String }, 
-    fullname: { type: String },
     email:    { type: String, unique: true, required: true },
-    password: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true},
+    address: { type: String },
+    city: { type: String, required: true},
+    state_province: { type: String },
+    postal_code: { type: String, required: true},
+    country_code:   { type: String},
+    hidden: { type: Boolean },
+    password: { type: String, required: true }
   }
 })
 
