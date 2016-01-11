3ed6e4b38a74eb68651c242e4b514473415373c7
psbadewal <psbadewal@icloud.com>
2016-01-11 16:31:05 +0000

    Prevent requiring sign scraper without params

--- tasks/scraper.js
+++ tasks/scraper.js
@@ -1,6 +1,5 @@
 // Require scrapers
 var all  = require("./all");
-var sign = require("./sign");
 
 var mongoose       = require('mongoose');
 var config         = require('../config/config');
@@ -9,4 +8,3 @@ mongoose.connect(config.database);
 
 // Get everything from the url: https://www.change.org/petitions.json
 all();
-// sign();
