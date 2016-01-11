2ffa2728cdddf23715b7c1511af1f205480ea830
psbadewal <psbadewal@icloud.com>
2016-01-11 16:20:09 +0000

    Adding mongoose connect into scraper

--- tasks/scraper.js
+++ tasks/scraper.js
@@ -2,6 +2,11 @@
 var all  = require("./all");
 var sign = require("./sign");
 
+var mongoose       = require('mongoose');
+var config         = require('../config/config');
+
+mongoose.connect(config.database);
+
 // Get everything from the url: https://www.change.org/petitions.json
 all();
 // sign();
